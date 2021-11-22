ade13f672437e41199e281b48624d3d0b8bc7f71
Pedro Guerra <pdrowr@gmail.com>
2021-11-22 09:02:53 -0400

    Added incident status codes to shipment info

--- lib/deliveries/couriers/envialia/shipments/trace/format_response.rb
+++ lib/deliveries/couriers/envialia/shipments/trace/format_response.rb
@@ -30,10 +30,39 @@ module Deliveries
               "16" => 'Pendiente de emisión',
             }.freeze
 
+            INCIDENT_CODES = {
+              "C01" => 'Estacionado en plataforma',
+              "C02" => 'Devuelto por embalaje insuficiente',
+              "C03" => 'Mercancía pendiente de revisión',
+              "C04" => 'Modificado por Dpto. ATC',
+              "C05" => 'Mercancía irregular no permitida',
+              "D01" => 'Destinatario ausente / Local cerrado',
+              "D04" => 'Rehusado / Rechazado',
+              "D05" => 'Dirección incorrecta',
+              "D06" => 'No recepcionan / Pte. Fecha de entrega',
+              "D07" => 'No paga cobros',
+              "D09" => 'No paga CABILDOS/ADUANA',
+              "D11" => 'Retorno no preparado',
+              "D12" => 'Pendiente recoger destinatario en delegación',
+              "D13" => 'Reparto al día siguiente',
+              "D14" => 'Robo en destino',
+              "D18" => 'No entregar',
+              "D19" => 'Entrega reclamada',
+              "P01" => 'Pendiente de llegada',
+              "P02" => 'Expedición incompleta',
+              "P03" => 'Envío mal canalizado',
+              "P04" => 'Recibido en mal estado',
+              "P05" => 'Falta factura proforma',
+              "P06'" => 'Demora en llegada de vuelo / barco',
+              "P07" => 'Retenido en ADUANA',
+              "Z99" => 'Incidencia automática por actuación'
+            }
+
             def execute
               body = response.dig("Envelope", "Body", "WebServService___ConsEnvEstadosResponse", "strEnvEstados")
               parsed_response = Hash.from_xml(body).dig("CONSULTA", "ENV_ESTADOS")
               status = STATUS_CODES[parsed_response.dig("V_COD_TIPO_EST")]
+              status = INCIDENT_CODES[shipment_status.dig("V_COD_TIPO_EST")] if status.eql?('Incidencia')
 
               tracking_info_params = {}
               tracking_info_params[:courier_id] = 'envialia'
@@ -61,6 +90,7 @@ module Deliveries
 
             def formatted_checkpoint(shipment_status)
               status = STATUS_CODES[shipment_status.dig("V_COD_TIPO_EST")]
+              status = INCIDENT_CODES[shipment_status.dig("V_COD_TIPO_EST")] if status.eql?('Incidencia')
 
               date = shipment_status.dig("D_FEC_HORA_ALTA")
 
